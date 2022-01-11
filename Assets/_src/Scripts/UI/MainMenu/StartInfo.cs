1658dc6248ecfccc02930e52bf00c01d48417e56
Junger <naydanov_a@mail.ru>
2022-01-11 13:02:54 +0500

    edit StartInfo

--- Assets/_src/Scripts/UI/MainMenu/StartInfo.cs
+++ Assets/_src/Scripts/UI/MainMenu/StartInfo.cs
@@ -4,25 +4,22 @@ using System.Collections.Generic;
 using BurgerHeroes.Analytics;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 
 namespace BurgerHeroes.UI
 {
-    public class StartInfo : MonoBehaviour
+    public class StartInfo : MonoBehaviour, IPointerDownHandler
     {
         [SerializeField, SceneObjectsOnly, Required]
         private MainMenu _mainMenu;
-        
-        
-        private void Update()
+
+        public void OnPointerDown(PointerEventData eventData)
         {
-            if (UnityEngine.Input.GetMouseButtonDown(0))
-            {
-                AmplitudeManager.Instance.SendLevelStartEvent();
-                AmplitudeManager.Instance.SetLastLevel();
-                
-                _mainMenu.StartGameplay();
-            }
+            AmplitudeManager.Instance.SendLevelStartEvent();
+            AmplitudeManager.Instance.SetLastLevel();
+
+            _mainMenu.StartGameplay();
         }
     }   
 }
